b3faa481d2231cf8c3e4e47cb5721c018abb894a
thelordcat-me <thelordcat.me+github@gmail.com>
2026-09-12 13:16:22 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
