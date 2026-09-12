cd07614444fa733a241369063c4c377b256714f0
thelordcat-me <thelordcat.me+github@gmail.com>
2026-09-12 13:47:19 +0000

    Ch3 : L1

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
