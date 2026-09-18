48261729bb5624d6769049d719dcbf9328ae99b1
thelordcat-me <thelordcat.me+github@gmail.com>
2026-09-18 12:43:29 +0000

    Bullet dispaly added

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
 import pygame
 from player import Player 
-from circleshape import CircleShape
-from constants import SCREEN_WIDTH
-from constants import SCREEN_HEIGHT
-from logger import log_state
+from asteroid import Asteroid
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state, log_event
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     x: int = SCREEN_WIDTH / 2
     y: int = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots,updatable,drawable)
+
+
     player = Player(x,y)
-
-    print(f"Starting Asteroids")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    clock = pygame.time.Clock()
+    AsteroidField()
+  
     dt = 0.0
+
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
+            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("game over!")
+                sys.exit()
+
+        screen.fill("black") 
+
+        for obj in drawable:
+            obj.draw(screen)   
+        pygame.display.flip()      
+
         dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,11 +1,12 @@
-from constants import PLAYER_RADIUS
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED,PLAYER_SHOOT_SPEED
 import pygame
 from circleshape import CircleShape
+from shot import Shot
+
 class Player(CircleShape):
     def __init__(self,x: int,y: int) ->None:
         super().__init__(x,y,PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 0.0
 
     # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
@@ -16,6 +17,37 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def draw(self, screen) -> None:
-        point_list = self.triangle()
-        pygame.draw.polygon(screen,"white",point_list,)
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen,"white",self.triangle(),LINE_WIDTH)
+
+    def rotate(self,dt: float) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0,1)
+        rotate_vector = unit_vector.rotate(self.rotation)
+        rotate_speed_vector = rotate_vector * PLAYER_SPEED * dt
+        self.position += rotate_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position[0],self.position[1])
+        unit_vector = pygame.Vector2(0,1)
+        rotate_vector = unit_vector.rotate(self.rotation)
+        shot_speed = rotate_vector * PLAYER_SHOOT_SPEED
+        #print(shot_speed)
+        shot.velocity = shot_speed
+       
